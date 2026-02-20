f240c0de67e8fc39db40d6e4dd6d4fb145612376
Matheus <Omatheusaugusto90@gmail.com>
2026-02-20 14:01:28 -0300

    vamos que vamos

---
+++ WebApplicationPods/Areas/PainelLojista/Controllers/ProdutoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationPods.Data;
+
+namespace WebApplicationPods.Areas.PainelLojista.Controllers
+{
+    [Area("PainelLojista")]
+    public class ProdutoController : Controller
+    {
+        private readonly BancoContext _context;
+
+        public ProdutoController(BancoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Detalhes(int id)
+        {
+            var produto = _context.Produtos
+                .AsNoTracking()
+                .FirstOrDefault(p => p.Id == id);
+
+            if (produto == null) return NotFound();
+
+            return View(produto);
+        }
+    }
+}
